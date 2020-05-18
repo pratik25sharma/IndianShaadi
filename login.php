46a3c48d72968e9fcf16039b1e0efdf95156e699
satish <niakumar2050@rediffmail.com>
2020-05-18 20:03:15 +0530

    commit fist

--- login.php
+++ login.php
@@ -11,7 +11,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Rishtey Indian - Login</title>
+	<title>Rishtey  Indian - Login</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/login.css">
 	<link rel="stylesheet" type="text/css" href="css/media.css">
